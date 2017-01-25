12d2c43229f9a631011f4cdf554f25bc66884907
David Lavoie-Boutin <david.lavoie-boutin@mail.mcgill.ca>
2017-01-25 17:31:34 -0500

    Fix startup routine

--- Lab1_STM32F4Cube_Base_project/RTE/Device/STM32F407VGTx/startup_stm32f407xx.s
+++ Lab1_STM32F4Cube_Base_project/RTE/Device/STM32F407VGTx/startup_stm32f407xx.s
@@ -187,8 +187,16 @@ Reset_Handler    PROC
         IMPORT  SystemInit
         IMPORT  __main
 
-                 LDR     R0, =SystemInit
-                 BLX     R0
+ ;                 LDR     R0, =SystemInit
+ ;                 BLX     R0
+ 
+								 LDR.W  R0, =0xE000ED88
+								 LDR R1, [R0]
+								 ORR R1, R1, #(0xF << 20)
+								 STR R1, [R0]
+								 DSB
+								 ISB
+								
                  LDR     R0, =__main
                  BX      R0
 				 NOP
