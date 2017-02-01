62d5abcb251da6abad6fa66f4c33150d9434217c
Malcolm Watt <malcolm.watt@mail.mcgill.ca>
2017-02-01 14:04:37 -0500

    Start outer loop and add pseudocode.

--- Lab1_STM32F4Cube_Base_project/Sources/Assembly.s
+++ Lab1_STM32F4Cube_Base_project/Sources/Assembly.s
@@ -22,19 +22,57 @@ AVERAGE_asm PROC
 	; 	R1 Pointer to coefficient array
 	; 	R2 Pointer to output array
 	;	R3 Length of coefficient array
-	LDR		R5, = 0		; Initialize loop counter
+	;	R4 Length of the output array
+	
+	; Outer loop: Goes through all the data segments.
+	LDR		R6, = 0		; Initialize the outer loop counter.
+	
+	; Set R10 such that it is 1 less than the number of words in coefficient array
+	; in bytes, hence the multiplication. This will allow us to shift back our data
+	; segment at the end of the inner loop.
+	LDR 	R10, = 4 	; Initialize R10
+	
+	; Multiply the number of bytes per word by the 
+	; length of the coeff array and subtract from 1
+	; to give appropriate shift
+	MLS		R10, R10, R3, = 1	
+
+outer_loop
+	;initialize the counter of the outer loop .. done
+	;set the ammount that we have to shift back by .. done
+
+	;Outer loop
+		;exit if we have exceeded the outer loop counter. 
+
+		;Take the average starting at the position based on the outer loop
+		;counter as an offset.
+		
+		;Set the current output register to the result
+
+		;Increment the address of the output register	
+
+		;Increment the outer loop counter.
+
+		;Set the Data pointer register to the starting value plus offset
+
+		;Reset to coefficient pointer back to the start of the array
+	
+	LDR		R5, = 0		; Initialize averaging_loop counter
 	VSUB.F32 S0, S0, S0	; Initialize total ; TODO: FIX THIS HACK INITIALISATION 
-loop
+averaging_loop
 	CMP		R5, R3 		; Compare loop index to coeffs length
-	BEQ 	exit 		; If reached loop end branch out
+	BEQ 	exit_averaging_loop 		; If reached loop end branch out
 	VLDR.F32 	S1, [R0] 	; Load next data element
 	VLDR.F32	S2, [R1]	; Load next coefficient
 	VFMA.F32	S0, S1, S2 	; Multiply data and coeff, then accumulate on total
 	ADD R5, R5, #0x1 	; Increment index
 	ADD R0, R0, #0x4 	; Shift data pointer to next element
 	ADD R1, R1, #0x4 	; Shift data pointer to next element
-	B 			loop 		; Jump to beginning of loop
-exit
+	B 			averaging_loop 		; Jump to beginning of loop
+exit_averaging_loop
+
+
+exit_outer_loop
 	
 	VSTR.F32	S0, [R2] 	; Write back floating point result to memory
 	BX LR ; Return from function call
