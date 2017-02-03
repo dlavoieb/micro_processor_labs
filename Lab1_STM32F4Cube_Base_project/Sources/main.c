a36ee192d2bac2b5f59017b1a86b2536a93b5e12
Malcolm Watt <malcolm.watt@mail.mcgill.ca>
2017-02-03 12:47:35 -0500

    Improve readability of main.c.

--- Lab1_STM32F4Cube_Base_project/Sources/main.c
+++ Lab1_STM32F4Cube_Base_project/Sources/main.c
@@ -29,9 +29,11 @@ int main()
         data[i] = i;
     }
     
-    //---------------------------
-    //---Custom implementation---
-    //---------------------------
+    // ==== PART A ====
+
+    //-----------------------------
+    //---Custom C implementation---
+    //-----------------------------
     int data_index = COEFFS_LENGTH;    
     while (data_index < COEFFS_LENGTH + DATA_LENGTH) {
         //build a window
@@ -61,16 +63,14 @@ int main()
     uint32_t blockSize = BLOCK_SIZE;
     uint32_t numBlocks = DATA_LENGTH/BLOCK_SIZE;
 
-    /* Initialize input and output buffer pointers */
+    // Initialize input and output buffer pointers
     inputF32 = &data[5];
     outputF32 = &output[0];
 
-    /* Call FIR init function to initialize the instance structure. */
+    // Call FIR init function to initialize the instance structure.
     arm_fir_init_f32(&S, NUM_TAPS, (float32_t *)&coeffs[0], &firStateF32[0], blockSize);
 
-    /* ----------------------------------------------------------------------
-     ** Call the FIR process function for every blockSize samples
-     ** ------------------------------------------------------------------- */
+    // Call the FIR process function for every blockSize samples
     for(int i=0; i < numBlocks; i++) {
       arm_fir_f32(&S, inputF32 + (i * blockSize), outputF32 + (i * blockSize), blockSize);
     }
@@ -78,13 +78,15 @@ int main()
     print_output(outputF32, DATA_LENGTH);
     
     
-    //------------------------
-    //--part b data analysis--
-    //------------------------
+    // ==== Part b ====
     
     float32_t difference[DATA_LENGTH];
     float32_t avg;
     
+    // -------------------------------
+    // --- Custom C Implementation ---
+    // -------------------------------
+
     // Subtraction of original data stream and data obtained by filter tracking.
     datastream_substraction(&data[COEFFS_LENGTH], output, difference, DATA_LENGTH);
     print_data(difference, DATA_LENGTH);
@@ -93,15 +95,17 @@ int main()
     float32_t std_dev = std_deviation(difference, &avg, DATA_LENGTH);
     printf("STD DEVIATION: %f\nAVERAGE: %f\n", std_dev, avg);
 
-    /* 
-     * Calculates the correlation between original and tracked vectors.
-     */
+
+    // Calculates the correlation between original and tracked vectors.
     float32_t correlation_data[DATA_LENGTH * 2 - 1];
     correlation(correlation_data, &data[COEFFS_LENGTH], output, DATA_LENGTH);
     printf("Calculate correlation :");
     print_output(correlation_data, DATA_LENGTH);
   
-    // --- CMSIS DATA ANALYSIS
+    // ---------------------------
+    // --- CMSIS DATA ANALYSIS ---
+    // ---------------------------
+
     arm_sub_f32 (&data[COEFFS_LENGTH], output, difference, DATA_LENGTH);
     printf("Difference from CMSIS: ");
     print_output(difference, DATA_LENGTH);
@@ -117,6 +121,10 @@ int main()
     while(1);
 }
 
+
+/**
+ * @brief Printing helper for data.
+ */
 void print_data(float32_t * data, size_t length) {
     printf("Data array: \n[");
     for (int i = 0; i < length; i++) {
@@ -125,6 +133,10 @@ void print_data(float32_t * data, size_t length) {
     printf("]\n");
 }
 
+
+/**
+ * @brief Printing helper for output.
+ */
 void print_output(float32_t* output, size_t length) {
     printf("Output values: \n[");
     for (int i = 0; i < length; i++) {
@@ -135,7 +147,7 @@ void print_output(float32_t* output, size_t length) {
 
 
 /**
- * Compute the weighted average of a data set
+ * @brief Compute the weighted average of a data set.
  */
 float32_t average(float * data, float * coeffs, size_t length) {
     float prod_sum = 0;
@@ -146,12 +158,20 @@ float32_t average(float * data, float * coeffs, size_t length) {
     return prod_sum;
 }
 
+
+/**
+ * @brief Set the output to the vectorized subtraction of data_two from data_one.
+ */
 void datastream_substraction(float32_t * data_one, float32_t * data_two, float32_t * output, size_t length) {
     for (int i = 0; i < length; i++) {
         output[i] = data_one[i] - data_two[i];
     }
 }
 
+
+/**
+ * @brief Calculates the average and standard deviation of data set.
+ */
 float32_t std_deviation(float32_t* data, float32_t* avg, size_t length) {
     // average of dataset
     float32_t sum = 0;
@@ -168,11 +188,11 @@ float32_t std_deviation(float32_t* data, float32_t* avg, size_t length) {
     return sqrt(total_deviation/length);
 }
 
+
+/**
+ * @brief Calculates the correlation between and data and filtered_data.
+ */
 void correlation(float32_t * correlated, float32_t * data, float32_t * filtered_data, size_t length) {
-    /*
-     * Conceptually the same as convolution, except we do not flip the second data set the way we do in convolution.
-     */
-    
     // Phase 1, the data is being overlapped from the left by the filtered data set.
     int index = 0;
     int overlap;
