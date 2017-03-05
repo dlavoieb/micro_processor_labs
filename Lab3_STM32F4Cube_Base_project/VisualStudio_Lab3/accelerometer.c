224740eb26cfe72d715df6e9dd2570845e149af3
Malcolm Watt <malcolm.watt@mail.mcgill.ca>
2017-03-04 19:54:31 -0500

    Configure accelerometer.

---
+++ Lab3_STM32F4Cube_Base_project/VisualStudio_Lab3/accelerometer.c
@@ -0,0 +1,86 @@
+/**
+ * @file	accelerometer.c
+ * @author	Malcolm Watt
+ * @version	0.0.1
+ * @date	2017-02-24
+ * @brief	Configuration and implementation of ISR for accelerometer data.
+ */
+
+#include "accelerometer.h"
+
+void accelerometer_init(void)
+{
+	// Sets the data rate for the accelerometer.
+	sensor_config();
+	
+	// Sets up interrupt type for the accelerometer.
+	interrupt_generation_config();
+	
+	// Configure GPIOs for interrupts.
+	interrupt_GPIO_config();
+	
+	// Configure and enable the interrupt through NVIC module.
+	HAL_NVIC_SetPriority(EXTI0_IRQn, 1, 0);
+}
+
+void EXTI0_IRQHandler(void)
+{
+}
+
+void sensor_config(void)
+{
+	// Generate a LIS3DSH init construct, and set the appropriate values for our application.
+	LIS3DSH_InitTypeDef LIS3DSH_InitStruct;
+
+	// Set data rate to 100 Hz as this is approximately the refresh rate of our display.
+	LIS3DSH_InitStruct.Power_Mode_Output_DataRate = LIS3DSH_DATARATE_25;
+
+	// This leaves the BDU feature unset. Acceptable since reading the angular data is not a speed bottleneck.
+	LIS3DSH_InitStruct.Continous_Update = LIS3DSH_ContinousUpdate_Disabled;
+
+	// Enable all three axes of the accelerometer since they are all required for angle determination.
+	LIS3DSH_InitStruct.Axes_Enable = LIS3DSH_XYZ_ENABLE;
+
+	// The following are settings that are slightly arbitrary.
+	LIS3DSH_InitStruct.AA_Filter_BW = LIS3DSH_AA_BW_800;
+	LIS3DSH_InitStruct.Full_Scale = LIS3DSH_FULLSCALE_2;
+	LIS3DSH_InitStruct.Self_Test = LIS3DSH_SELFTEST_NORMAL;
+
+	// Call the configuration function for the accelerometer.
+	LIS3DSH_Init(&LIS3DSH_InitStruct);
+}
+
+void interrupt_generation_config(void)
+{
+	// Initialise the interupt configuration struct.
+	LIS3DSH_DRYInterruptConfigTypeDef LIS3DSH_InterruptConfig;
+	
+	// Enable interupts.
+	LIS3DSH_InterruptConfig.Dataready_Interrupt = LIS3DSH_DATA_READY_INTERRUPT_ENABLED;
+	
+	// Make the interupt signal active high.
+	LIS3DSH_InterruptConfig.Interrupt_signal = LIS3DSH_ACTIVE_HIGH_INTERRUPT_SIGNAL;
+	
+	// Use pulsed interupt signals.
+	LIS3DSH_InterruptConfig.Interrupt_type = LIS3DSH_INTERRUPT_REQUEST_PULSED;
+	
+	// Call the configuration function for the interupts.
+	LIS3DSH_DataReadyInterruptConfig(&LIS3DSH_InterruptConfig);
+}
+
+void interrupt_GPIO_config(void)
+{
+	__HAL_RCC_GPIOE_CLK_ENABLE();
+
+	// Explicitly reset port A0 (push button port) to avoid conflicts.
+	HAL_GPIO_DeInit(GPIOA, GPIO_PIN_0);
+	
+	// Setup GPIO port E0 for accelerometer interrupts.
+	GPIO_InitTypeDef GPIO_InitStruct;
+	GPIO_InitStruct.Pin = GPIO_PIN_0;
+	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+	GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+	GPIO_InitStruct.Pull = GPIO_PULLDOWN;
+	
+	HAL_GPIO_Init(GPIOE, &GPIO_InitStruct);
+}
---
+++ Lab3_STM32F4Cube_Base_project/VisualStudio_Lab3/accelerometer.h
@@ -0,0 +1,51 @@
+/**
+ * @file	accelerometer.h
+ * @author	Malcolm Watt
+ * @version	0.0.1
+ * @date	2017-02-24
+ * @brief	Configuration and implementation of ISR for accelerometer data.
+ */
+
+#ifndef ACCELEROMETER_H_
+#define ACCELEROMETER_H_
+
+#include "LIS3DSH.h"
+#include "stm32f4xx_hal.h"
+
+/**
+ * @author	Malcolm Watt
+ * @brief	Initialize the LIS3DSH accelerometer, interupts, and GPIO.
+ * @description	This function is the only function that needs to be called externally to setup the accelerometer
+ *				for use with interrupts. The interrupt service routine is implemented in: PLACEHOLDER.
+ */
+void accelerometer_init(void);
+
+/**
+ * @author	Malcolm Watt
+ * @brief	Interrupt handler on external line0 for the accelerometer.
+ */
+void EXTI0_IRQHandler(void);
+
+/**
+ * @author	Malcolm Watt
+ * @brief	Configure the LIS3DSH accelerometer.
+ * @description	Internally calls the LIS3DSH library functin for setting up the accelerometer. The main concern
+ *				for this is the data rate, which is set to 25 Hz.
+ */
+void sensor_config(void);
+
+/**
+ * @author	Malcolm Watt
+ * @brief	Configure the generation of interupts by the accelerometer.
+ * @description	Internally calls the LIS3DSH library function for setting up the accelerometer. The setup
+ *				makes the interrupt signals active high and non-latched.
+ */
+void interrupt_generation_config(void);
+
+/**
+ * @author	Malcolm Watt
+ * @brief	Set up the hardwired E0 GPIO for interupts. Explicitly reset the conflicting A0 GPIO.
+ */
+void interrupt_GPIO_config(void);
+
+#endif // !ACCELEROMETER_H_
