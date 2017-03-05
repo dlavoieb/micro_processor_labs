4071d8ee2ed4a697d3363eb71793d2e1cac49155
David Lavoie-Boutin <d.lavoie.boutin@me.com>
2017-03-05 13:11:30 -0500

    Fix abort data entry timer. If '*' is held for 3 secnds, the data entry is cancelled

--- Lab3_STM32F4Cube_Base_project/VisualStudio_Lab3/timer.c
+++ Lab3_STM32F4Cube_Base_project/VisualStudio_Lab3/timer.c
@@ -3,7 +3,7 @@
 #include "supporting_functions.h"
 
 TIM_HandleTypeDef TIM_Handle2;
-uint8_t timer2_flag;
+volatile uint8_t timer2_flag;
 uint8_t display_timer_flag;
 void init_timers(void)
 {
@@ -11,9 +11,9 @@ void init_timers(void)
 	
 	// PERIOD*PRESCALAR CANNOT EXCEED 32BIT (TIM2)
 	TIM_Handle2.Instance				= TIM2;
-	TIM_Handle2.Init.ClockDivision		= 0;
-	TIM_Handle2.Init.Prescaler 			= 999;														// PRESCALAR OF 999
-	TIM_Handle2.Init.Period 			= 6999;														// PERIOD OF 6999 FOR OPTIMAL POLLING RATE OF KEYPAD
+	TIM_Handle2.Init.ClockDivision		= TIM_CLOCKDIVISION_DIV4;
+	TIM_Handle2.Init.Prescaler 			= 0x1000U;														// PRESCALAR OF 999
+	TIM_Handle2.Init.Period 			= 0xFFFFU;														// PERIOD OF 6999 FOR OPTIMAL POLLING RATE OF KEYPAD
 	TIM_Handle2.Init.CounterMode 		= TIM_COUNTERMODE_UP;
 	TIM_Handle2.Init.ClockDivision 		= TIM_CLOCKDIVISION_DIV1;
 	TIM_Handle2.Init.RepetitionCounter 	= 0;
@@ -23,6 +23,12 @@ void init_timers(void)
 		
 	HAL_NVIC_SetPriority(TIM2_IRQn, 2, 1);
 	HAL_NVIC_EnableIRQ(TIM2_IRQn);
+	HAL_TIM_Base_Start_IT(&TIM_Handle2);
+}
+
+void TIM2_IRQHandler(void)
+{
+	HAL_TIM_IRQHandler(&TIM_Handle2);
 }
 
 // CALLBACK FUNCTION, TIME THE IRQHANDLER IS CALLED
@@ -37,11 +43,11 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
 
 void start_abort_timer(void)
 {
-	//HAL_TIM_Base_Start_IT(&TIM_Handle2);
+	HAL_TIM_Base_Start_IT(&TIM_Handle2);
 }
 
 void stop_abort_timer(void)
 {
-	//HAL_TIM_Base_Stop_IT(&TIM_Handle2);
+	HAL_TIM_Base_Stop_IT(&TIM_Handle2);
 }
 	
